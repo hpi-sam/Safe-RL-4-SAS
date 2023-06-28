3e5fc7fd0c63815f1a0f046ea6c6f1849b4a6066
Finn Kaiser <37416935+Finnk11@users.noreply.github.com>
2023-06-28 12:30:15 +0200

    DQN agent implemented and runnable

--- experiments/dqn_simple_intersection.py
+++ experiments/dqn_simple_intersection.py
@@ -1,3 +1,4 @@
+import glob
 import os
 import sys
 
@@ -31,6 +32,11 @@ def main_gym():
 
 
 def train():
+
+    filelist = glob.glob(os.path.join('outputs', 'simple_intersection', "*.csv"))
+    for f in filelist:
+        os.remove(f)
+
     env = SumoEnvironment(
         net_file="nets/simple_intersection/simple_intersection.net.xml",
         route_file="nets/simple_intersection/simple_intersection.rou.xml",
@@ -58,28 +64,43 @@ def train():
     model.save('models/dqn')
 
 
+
 def run():
+
+    additional_sumo_cmd = ['--d', '0']
+    # additional_sumo_cmd.extend(["--tripinfo-output", "data/dqn_simple_intersection/tripinfo.xml"])
+    # additional_sumo_cmd.extend(["--collision-output", "data/dqn_simple_intersection/collision.xml"])
+    additional_sumo_cmd.extend(["--collision.check-junctions"])
+
     env = SumoEnvironment(
         net_file="nets/simple_intersection/simple_intersection.net.xml",
         route_file="nets/simple_intersection/simple_intersection.rou.xml",
-        out_csv_name="outputs/simple_intersection/dqn",
+        out_csv_name="outputs/simple_intersection/dqn_100000",
         single_agent=True,
         use_gui=True,
+        sumo_warnings=True,
         num_seconds=3600,
-        # additional_sumo_cmd=f"--delay 100 --collision.check-junctions"
+        additional_sumo_cmd=' '.join(additional_sumo_cmd)
     )
-    model = DQN.load('models/dqn.zip')
+    model = DQN.load('models/dqn_100000.zip', env=env)
 
-    obs, state = env.reset()
+    obs, _info = env.reset()
     traci.gui.setOffset(traci.gui.DEFAULT_VIEW, x=125, y=100)
     traci.gui.setZoom(traci.gui.DEFAULT_VIEW, 350)
 
     done = False
     while not done:
-        action, _states = model.predict(obs, state)
-        print(action)
-        next_obs, reward, terminated, truncated, state = env.step(action)
+        action, _states = model.predict(obs, deterministic=True)
+        obs, reward, terminated, truncated, info = env.step(action)
         env.render()
+
+        current_collisions = traci.simulation.getCollisions()
+        if current_collisions:
+            print(current_collisions)
+
+        if info['step'] == 500:
+            print(info)
+            pass
         done = terminated or truncated
 
 
